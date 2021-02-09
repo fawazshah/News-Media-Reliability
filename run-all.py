b9d138fd0540450e41999a481ed07576e66f2710
Fawaz Shah <fawaz010@gmail.com>
2021-02-09 00:46:41 +0000

    Modify run-all

--- run-all.py
+++ run-all.py
@@ -4,12 +4,17 @@ import sys
 
 if __name__ == "__main__":
 
-    args = " ".join(sys.argv[1:])
-    print("USING SVM")
-    os.system(f"python3 classifiers/svc.py {args}")
-    print("USING MLP")
-    os.system(f"python3 classifiers/mlp.py {args}")
-    print("USING DECISION TREE")
-    os.system(f"python3 classifiers/decision-tree.py {args}")
-    print("USING RANDOM FOREST")
-    os.system(f"python3 classifiers/random-forest.py {args}")
+    args_list = ["-tk 'bias' -f 'ALL'", "-tk 'bias' -f 'ARTICLE_ALL'"]
+    for args in args_list:
+        print("USING SVM")
+        os.system(f"python3 classifiers/svc.py {args}")
+        print("USING MLP")
+        os.system(f"python3 classifiers/mlp.py {args}")
+        print("USING DECISION TREE")
+        os.system(f"python3 classifiers/decision-tree.py {args}")
+        print("USING RANDOM FOREST")
+        os.system(f"python3 classifiers/random-forest.py {args}")
+        # print("USING GRADIENT BOOSTED TREES")
+        # os.system(f"python3 classifiers/gradient-boosted-forest.py {args}")
+        print("USING ADABOOST")
+        os.system(f"python3 classifiers/adaboost.py {args}")
