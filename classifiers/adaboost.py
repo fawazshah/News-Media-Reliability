6980887811f222425a5f3e3ebfc52f0d298ec9a5
Fawaz Shah <fawaz010@gmail.com>
2021-02-09 00:42:48 +0000

    Add adaboost

---
+++ classifiers/adaboost.py
@@ -0,0 +1,33 @@
+import numpy as np
+from sklearn.ensemble import AdaBoostClassifier
+
+from train import train_model
+
+
+class AdaBoostWrapper:
+
+    def __init__(self):
+        pass
+
+    @staticmethod
+    def get_name():
+        return "adaboost"
+
+    @staticmethod
+    def get_classifier():
+        return AdaBoostClassifier()
+
+    @staticmethod
+    def get_gridsearch_params():
+        return dict(learning_rate=np.logspace(-4, 1, 6),
+                    n_estimators=np.arange(50, 250, 50),
+                    )
+
+    @staticmethod
+    def get_best_classifier(clf):
+        return AdaBoostClassifier(
+            learning_rate=clf.best_estimator_.learning_rate,
+            n_estimators=clf.best_estimator_.n_estimators,
+        )
+
+train_model(AdaBoostWrapper)
